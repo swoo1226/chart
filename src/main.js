26bcfcbe43c742c4043fd92ec79b7def844dce4d
Peter Kim <sangwoo.kim@earlysloth.com>
2019-11-04 17:19:14 +0900

    made a random chart

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import {Bar} from 'vue-chartjs'
 
 Vue.config.productionTip = false
-
+export default {
+  extends: Bar,
+}
 new Vue({
   render: h => h(App),
 }).$mount('#app')
