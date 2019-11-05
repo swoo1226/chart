a52026e70fea8aacf8bb1067e8555c2b3ab25ddb
Peter Kim <sangwoo.kim@earlysloth.com>
2019-11-05 15:51:41 +0900

    Vue Apexcharts Basic
      main.js에 VueApexCharts 모듈의 내용을 사용할 수 있게 코드 삽입
      apexchart 컴포넌트를 사용하여 그래프 그리기 가능해짐
      width, type, options, series 등으로 그래프 세부 조정 가능

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,52 @@
 <template>
   <div id="app">
-    <RandomChart />
+    <apexchart width="550" type="bar" :options="chartOptions" :series="series"></apexchart>
+     <div>
+       <button @click="updateChart">Update!</button>
+    </div>
   </div>
 </template>
 
 <script>
-import RandomChart from './components/RandomChart'
 
 export default {
   name: 'app',
-  components: {
-    RandomChart
-  }
+    data: function() {
+      return {
+        chartOptions: {
+          chart: {
+            id: 'vuechart-example',
+          },
+          xaxis: {
+            categories: [1991, 1992, 1993, 1994, 1995, 1996, 1997, 1998],
+          },
+        },
+        series: [{
+          name: 'series-1',
+          data: [30, 40, 45, 50, 49, 60, 70, 81]
+        }]
+      }
+    },
+    methods: {
+      updateChart() {
+        const max = 90;
+        const min = 20;
+        const newData = this.series[0].data.map(() => {
+          return Math.floor(Math.random() * (max - min + 1)) + min
+        })
+
+        const colors = ['#008FFB', '#00E396', '#FEB019', '#FF4560', '#775DD0']
+
+        // Make sure to update the whole options config and not just a single property to allow the Vue watch catch the change.
+        this.chartOptions = {
+          colors: [colors[Math.floor(Math.random()*colors.length)]]
+        };
+        // In the same way, update the series option
+        this.series = [{
+          data: newData
+        }]
+      }
+    }
 }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
 import {Bar} from 'vue-chartjs'
-
+import VueApexCharts from 'vue-apexcharts'
+Vue.use(VueApexCharts)
+Vue.component('apexchart', VueApexCharts)
 Vue.config.productionTip = false
 export default {
   extends: Bar,
